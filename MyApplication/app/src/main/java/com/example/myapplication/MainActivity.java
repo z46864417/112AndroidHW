586c98c713a3d881c9873920b87eacd70224d012
z46864417 <z46864417@gmail.com>
2024-06-14 13:00:30 +0800

    Graphic ordering
    Graphic ordering

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,79 +7,59 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<Integer> values = new ArrayList<>();
+    private int[] images = {R.mipmap.burger, R.mipmap.frenchfry, R.mipmap.softdrink, R.mipmap.soup, R.mipmap.coffee};
+    private ImageView[] outputs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckBox chk1 = findViewById(R.id.chk1);
-        CheckBox chk2 = findViewById(R.id.chk2);
-        CheckBox chk3 = findViewById(R.id.chk3);
-        CheckBox chk4 = findViewById(R.id.chk4);
-        CheckBox chk5 = findViewById(R.id.chk5);
-        ImageView output1 = findViewById(R.id.output1);
-        ImageView output2 = findViewById(R.id.output2);
-        ImageView output3 = findViewById(R.id.output3);
-        ImageView output4 = findViewById(R.id.output4);
-        ImageView output5 = findViewById(R.id.output5);
+        CheckBox[] chks = {
+                findViewById(R.id.chk1),
+                findViewById(R.id.chk2),
+                findViewById(R.id.chk3),
+                findViewById(R.id.chk4),
+                findViewById(R.id.chk5)
+        };
 
-        chk1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    output1.setImageResource(R.mipmap.burger);
-                } else {
-                    output1.setImageResource(0);
+        outputs = new ImageView[]{
+                findViewById(R.id.output1),
+                findViewById(R.id.output2),
+                findViewById(R.id.output3),
+                findViewById(R.id.output4),
+                findViewById(R.id.output5)
+        };
+
+        for (int i = 0; i < chks.length; i++) {
+            final int index = i;
+            chks[i].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        values.add(index);
+                    } else {
+                        values.remove(Integer.valueOf(index));
+                    }
+                    updateImages();
                 }
+            });
+        }
+    }
+
+    private void updateImages() {
+        for (int i = 0; i < outputs.length; i++) {
+            if (i < values.size()) {
+                outputs[i].setImageResource(images[values.get(i)]);
+            } else {
+                outputs[i].setImageResource(0);
             }
-        });
-
-        chk2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    output2.setImageResource(R.mipmap.frenchfry);
-                } else {
-                    output2.setImageResource(0);
-                }
-            }
-        });
-
-        chk3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    output3.setImageResource(R.mipmap.softdrink);
-                } else {
-                    output3.setImageResource(0);
-                }
-            }
-        });
-
-        chk4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    output4.setImageResource(R.mipmap.soup);
-                } else {
-                    output4.setImageResource(0);
-                }
-            }
-        });
-
-        chk5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    output5.setImageResource(R.mipmap.coffee);
-                } else {
-                    output5.setImageResource(0);
-                }
-            }
-        });
-
-
+        }
     }
 }
